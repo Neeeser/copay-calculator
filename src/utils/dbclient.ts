c4011fe1bb982e5e73e07585d9b2ae711c2e7dca
andrew <andrew.neeser@me.com>
2023-12-18 21:47:52 -0500

    Remove unused dbClient.ts file

--- src/utils/dbclient.ts
+++
@@ -1,7 +0,0 @@
-// In your dbClient.ts
-import { createClient } from '@vercel/postgres';
-
-// If your variable is not prefixed with PUBLIC_, it will not be exposed to the client-side.
-const connectionString = import.meta.env.POSTGRES_URL;
-
-export const db = createClient({ connectionString });
